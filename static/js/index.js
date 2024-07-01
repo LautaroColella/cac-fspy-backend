a0ea21f6ab888980491bbf0e3ba8c8d248cfd383
Lautaro Colella <lautarocolella1@gmail.com>
2024-06-30 21:55:48 -0300

    create emp works

--- static/js/index.js
+++ static/js/index.js
@@ -9,6 +9,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
 	const createEmployeeButton = document.getElementById('createEmployee');
 	createEmployeeButton.addEventListener('click', () => displayCreateForm());
+
+	const createEmployeeForm = document.getElementById('createEmployeeForm');
+	createEmployeeForm.addEventListener('submit', (e) => { handleCreateEmployee(e) });
+	createEmployeeForm.addEventListener('reset', () => getEmployees());
 	getEmployees();
 });
 
@@ -70,12 +74,32 @@ function handleSearchForm(e) {
 function displayCreateForm() {
 	hideAll();
 	const empCreate = document.querySelector('.createEmployeeSection');
-	empCreate.style.display = 'block';
-
-
+	empCreate.classList.remove('hideElem');
 }
 
-async function handleCreateEmployee(userInput) {
+async function handleCreateEmployee(e) {
+	e.preventDefault();
+
+	const createEmployeeForm = document.getElementById('createEmployeeForm');
+
+	const formData = new FormData(createEmployeeForm);
+	const formDataJSON = {};
+
+	formData.forEach((value, key) => {
+		formDataJSON[key] = value;
+	});
+
+	const userInput = {
+		"firstname": formDataJSON["firstname"],
+		"lastname": formDataJSON["lastname"],
+		"position": formDataJSON["position"],
+		"age": formDataJSON["age"],
+		"start_date": formDataJSON["start_date"],
+		"salary": formDataJSON["salary"],
+		"email": formDataJSON["email"],
+		"photo": formDataJSON["photo"]
+	};
+
 	const validInput = validateEmployee(userInput);
 	if (Object.keys(validInput).length === 0) {
 		return;
@@ -114,126 +138,6 @@ async function handleCreateEmployee(userInput) {
 	}
 }
 
-function validateEmployee(userInput) {
-	const namePattern = /^[A-Za-zÁáÉéÍíÓóÚúÑñ\s]+$/;
-	const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-	const datePattern = /^\d{4}-\d{2}-\d{2}$/;
-	const salaryPattern = /^\d{1,8}(\.\d{1,2})?$/;
-	const photoUrlPattern = /^(https?):\/\/[^\s$.?#].[^\s]*$/;
-	const photoExtPattern = /\.(jpg|jpeg|png|bmp)$/i;
-
-	if (!userInput.firstname) {
-		displayAlert(true, "El nombre es obligatorio");
-		return {};
-	}
-	const firstnameField = userInput.firstname.trim();
-	if (!namePattern.test(firstnameField)) {
-		displayAlert(true, "El nombre solo puede contener letras");
-		return {};
-	} if (firstnameField.length > 254) {
-		displayAlert(true, "El nombre no puede contener más de 254 carácteres");
-		return {};
-	}
-
-	if (!userInput.lastname) {
-		displayAlert(true, "El apellido es obligatorio");
-		return {};
-	}
-	const lastnameField = userInput.lastname.trim();
-	if (!namePattern.test(lastnameField)) {
-		displayAlert(true, "El apellido solo puede contener letras");
-		return {};
-	} if (lastnameField.length > 254) {
-		displayAlert(true, "El apellido no puede contener más de 254 carácteres");
-		return {};
-	}
-
-	if (!userInput.email) {
-		displayAlert(true, "El email es obligatorio");
-		return {};
-	}
-	const emailField = userInput.email.trim();
-	if (!emailPattern.test(emailField)) {
-		displayAlert(true, "El email es inválido");
-		return {};
-	} if (emailField.length > 254) {
-		displayAlert(true, "El email no puede contener más de 254 carácteres");
-		return {};
-	}
-
-	if (!userInput.position) {
-		displayAlert(true, "La posición es obligatoria");
-		return {};
-	}
-	const positionField = userInput.position.trim();
-	if (positionField.length > 254) {
-		displayAlert(true, "La posición no puede contener más de 254 carácteres");
-		return {};
-	}
-
-	if (!userInput.age) {
-		displayAlert(true, "La edad es obligatoria");
-		return {};
-	}
-	const ageField = userInput.age.trim();
-	if (isNaN(ageField)) {
-		displayAlert(true, "La edad debe ser un número");
-		return {};
-	} if (ageField.length < 18 || ageField.length > 100) {
-		displayAlert(true, "La edad debe ser entre 18 y 100 años");
-		return {};
-	}
-
-	if (!userInput.start_date) {
-		displayAlert(true, "La fecha de inicio es obligatoria");
-		return {};
-	}
-	const myDate = userInput.start_date.trim();
-	if (!datePattern.test(myDate)) {
-		displayAlert(true, "La fecha de inicio tiene un formato inválido");
-		return {};
-	}
-	const startField = new Date(myDate);
-	if (startField < new Date('1944-01-01') || startField > new Date()) {
-		displayAlert(true, "La fecha de inicio debe estar entre el 1 de enero de 1944 y la fecha actual");
-		return {};
-	}
-
-	if (!userInput.salary) {
-		displayAlert(true, "El salario es obligatorio");
-		return {};
-	}
-	const salaryField = userInput.salary.trim();
-	if (!salaryPattern.test(salaryField)) {
-		displayAlert(true, "El salario debe ser un número válido con hasta dos decimales");
-		return {};
-	}
-
-	if (!userInput.photo) {
-		displayAlert(true, "La URL de la foto es obligatoria");
-		return {};
-	}
-	const photoField = userInput.photo.trim();
-	if (!photoUrlPattern.test(photoField)) {
-		displayAlert(true, "La URL de la foto no es válida");
-		return {};
-	} if (!photoExtPattern.test(photoField)) {
-		displayAlert(true, "La foto debe ser un archivo con una extensión válida (jpg, jpeg, png, bmp)");
-		return {};
-	}
-
-	return {
-		"firstname": firstnameField,
-		"lastname": lastnameField,
-		"position": positionField,
-		"age": ageField,
-		"start_date": startField,
-		"salary": salaryField,
-		"email": emailField,
-		"photo": photoField,
-	};
-}
-
 function displayEditEmployee(employee) {
 	console.log('asd');
 }
@@ -259,7 +163,7 @@ async function deleteEmployee(empId, fullname) {
 function displayEmployees(empleados) {
 	hideAll();
 	const userListContainer = document.querySelector(".employeeList");
-	userListContainer.style.display = 'block';
+	userListContainer.classList.remove('hideElem');
 	userListContainer.innerHTML = '';
 
 	for (let empleado of empleados) {
@@ -353,13 +257,133 @@ function displayEmployees(empleados) {
 	}
 }
 
+function validateEmployee(userInput) {
+	const namePattern = /^[A-Za-zÁáÉéÍíÓóÚúÑñ\s]+$/;
+	const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+	const datePattern = /^\d{4}-\d{2}-\d{2}$/;
+	const salaryPattern = /^\d{1,8}(\.\d{1,2})?$/;
+	const photoUrlPattern = /^(https?):\/\/[^\s$.?#].[^\s]*$/;
+	const photoExtPattern = /\.(jpg|jpeg|png|bmp)$/i;
+
+	if (!userInput.firstname) {
+		displayAlert(true, "El nombre es obligatorio");
+		return {};
+	}
+	const firstnameField = userInput.firstname.trim();
+	if (!namePattern.test(firstnameField)) {
+		displayAlert(true, "El nombre solo puede contener letras");
+		return {};
+	} if (firstnameField.length > 254) {
+		displayAlert(true, "El nombre no puede contener más de 254 carácteres");
+		return {};
+	}
+
+	if (!userInput.lastname) {
+		displayAlert(true, "El apellido es obligatorio");
+		return {};
+	}
+	const lastnameField = userInput.lastname.trim();
+	if (!namePattern.test(lastnameField)) {
+		displayAlert(true, "El apellido solo puede contener letras");
+		return {};
+	} if (lastnameField.length > 254) {
+		displayAlert(true, "El apellido no puede contener más de 254 carácteres");
+		return {};
+	}
+
+	if (!userInput.email) {
+		displayAlert(true, "El email es obligatorio");
+		return {};
+	}
+	const emailField = userInput.email.trim();
+	if (!emailPattern.test(emailField)) {
+		displayAlert(true, "El email es inválido");
+		return {};
+	} if (emailField.length > 254) {
+		displayAlert(true, "El email no puede contener más de 254 carácteres");
+		return {};
+	}
+
+	if (!userInput.position) {
+		displayAlert(true, "La posición es obligatoria");
+		return {};
+	}
+	const positionField = userInput.position.trim();
+	if (positionField.length > 254) {
+		displayAlert(true, "La posición no puede contener más de 254 carácteres");
+		return {};
+	}
+
+	if (!userInput.age) {
+		displayAlert(true, "La edad es obligatoria");
+		return {};
+	}
+	const ageField = userInput.age.trim();
+	if (isNaN(ageField)) {
+		displayAlert(true, "La edad debe ser un número");
+		return {};
+	} if (ageField < 18 || ageField > 100) {
+		displayAlert(true, "La edad debe ser entre 18 y 100 años");
+		return {};
+	}
+
+	if (!userInput.start_date) {
+		displayAlert(true, "La fecha de inicio es obligatoria");
+		return {};
+	}
+	const myDate = userInput.start_date.trim();
+	if (!datePattern.test(myDate)) {
+		displayAlert(true, "La fecha de inicio tiene un formato inválido");
+		return {};
+	}
+	const startField = new Date(myDate);
+	if (startField < new Date('1944-01-01') || startField > new Date()) {
+		displayAlert(true, "La fecha de inicio debe estar entre el 1 de enero de 1944 y la fecha actual");
+		return {};
+	}
+
+	if (!userInput.salary) {
+		displayAlert(true, "El salario es obligatorio");
+		return {};
+	}
+	const salaryField = userInput.salary.trim();
+	if (!salaryPattern.test(salaryField)) {
+		displayAlert(true, "El salario debe ser un número válido con hasta dos decimales");
+		return {};
+	}
+
+	if (!userInput.photo) {
+		displayAlert(true, "La URL de la foto es obligatoria");
+		return {};
+	}
+	const photoField = userInput.photo.trim();
+	if (!photoUrlPattern.test(photoField)) {
+		displayAlert(true, "La URL de la foto no es válida");
+		return {};
+	} if (!photoExtPattern.test(photoField)) {
+		displayAlert(true, "La foto debe ser un archivo con una extensión válida (jpg, jpeg, png, bmp)");
+		return {};
+	}
+
+	return {
+		"firstname": firstnameField,
+		"lastname": lastnameField,
+		"position": positionField,
+		"age": ageField,
+		"start_date": startField.toISOString().slice(0, 10),
+		"salary": salaryField,
+		"email": emailField,
+		"photo": photoField,
+	};
+}
+
 function hideAll() {
 	const empList = document.querySelector('.employeeList');
-	empList.style.display = 'none';
+	empList.classList.add('hideElem');
 	const empCreate = document.querySelector('.createEmployeeSection');
-	empCreate.style.display = 'none';
+	empCreate.classList.add('hideElem');
 	const empEdit = document.querySelector('.editEmployeeSection');
-	empEdit.style.display = 'none';
+	empEdit.classList.add('hideElem');
 }
 
 let isShowingAlert = false;
